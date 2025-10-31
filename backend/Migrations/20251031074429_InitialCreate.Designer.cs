12d5ae349278f5a3cd7cec881a9f6a4edb8e8f7c
PAUL909-09 <jjpalmazan17@gmail.com>
2025-10-31 15:50:52 +0800

    Add JWT authentication and update database config
    Added JWT secret to appsettings.json and updated the database name in the connection string. Included authentication-related NuGet packages in the project file. Renamed migration files to reflect new timestamp.

--- backend/Migrations/20251031074429_InitialCreate.Designer.cs
+++ backend/Migrations/20251031074429_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using TaskManager.Data;
 namespace task_manager_api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20251031033426_InitialCreate")]
+    [Migration("20251031074429_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
